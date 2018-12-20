2875095713a54c00324a863d21231548766d1904
Peter Nguyen <peternguyen160@gmail.com>
2018-12-20 15:11:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var katzDeli = [];
  function takeANumber(line, name){
    line.push(name);
- 
-  
+
+
     console.log(`Welcome, ${name}. You are number ${line.length} in line.`);
    return `Welcome, ${name}. You are number ${line.length} in line.`;
  }
- 
+
   function nowServing(line) {
    if(!line.length) {
      console.log("There is nobody waiting to be served!")
      return "There is nobody waiting to be served!"
    } else {
- 
+
       return "Currently serving " + line.shift() + ".";
    }
  }
@@ -21,17 +21,17 @@ var katzDeli = [];
      return "The line is currently empty.";
    }
    var lineName = [];
- 
+
     for(var i=0; i<line.length; i++) {
      lineName.push(i+1 + ". "+ line[i]);
    }
    console.log("The line is currently: " + lineName)
    return "The line is currently: " + lineName.join(', ');
  }
- 
-  
-  
-  
+
+
+
+
   takeANumber(katzDeli, "Ada")
  takeANumber(katzDeli, "Grace")
  takeANumber(katzDeli, "Kent")
